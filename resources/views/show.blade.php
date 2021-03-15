5677847bbaa70627f2f82681b686247ec584fc65
ivann93 <ivan.pushev@hotmail.com>
2021-03-15 06:06:03 +0100

    Delete Button
    Delete Button added

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -30,17 +30,23 @@
       <th>Id</th>
       <th>Name</th>
 	  <th>Image</th>
-      <th>Reseller ID</th>
       <th>Serial Number</th>
       <th>Type</th>
+	  <th>Delete</th>
     </tr>
     <tr>
       <td> {{ $display->id }}</td>
   	  <td> {{ $display->name }}</td>
 	  <td><img class="display-file_path" src="/storage/uploads/{{ $display->file_path }}" /></td>
-      <td> {{ $display->reseller_id }}</td>
       <td> {{ $display->serial_number }}</td>
       <td> {{ $display->type }}</td>
+	  <form method="POST" action="{{ route('displays.destroy', $display->id) }}">
+        <td>
+			@method('DELETE')
+			@csrf
+			<button type="submit" class="button is-link">Delete Display</button>
+        </td> 
+      </form>
     </tr>
   </table>
 </div>
